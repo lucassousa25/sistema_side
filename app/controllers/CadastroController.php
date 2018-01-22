b394f6fad8ab669c93a8b6fe633ccfae512679a0
Lucas S. Oliveira <lucassousao2510@gmail.com>
2018-01-22 07:53:29 -0200

    aprimorando código CadastroController.php

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -25,7 +25,7 @@ class CadastroController extends \HXPHP\System\Controller
 	public function cadastrarAction()
 	{
 		$this->view->setFile('index')
-				->setTemplate(false);
+				->setTemplate(false)
 				->setTitle('SIDE | Cadastro');
 
 		$this->request->setCustomFilters(array(
