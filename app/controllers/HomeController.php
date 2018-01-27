15132e6f1a3c8c44d3dd287c2528fc413dc36709
Lucas S. Oliveira <lucassousao2510@gmail.com>
2018-01-27 16:44:07 -0200

    Configurando redirecionamento da home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,9 +2,16 @@
 
 class HomeController extends \HXPHP\System\Controller
 {
-	public function __construct()
+	public function __construct($configs)
 	{
-		parent::__construct();
+		parent::__construct($configs);
+
+		$this->load(
+			'Services\Auth',
+			$configs->auth->after_login,
+			$configs->auth->after_logout,
+			true
+		);
 	}
 
 	public function indexAction()
@@ -13,5 +20,7 @@ class HomeController extends \HXPHP\System\Controller
                ->setHeader('header_side')
                ->setFooter('footer_side')
                ->setTemplate(true);
+
+        $this->auth->redirectCheck(false); //Configuração de redirecionamento de páginas (private/public)
 	}
 }
