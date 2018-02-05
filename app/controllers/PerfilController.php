2a6f07d4846aad18d83210f1e864ae27cdcac9c7
Lucas S. Oliveira <lucassousao2510@gmail.com>
2018-02-05 02:50:17 -0200

    Implementação inicial função de perfil

--- app/controllers/PerfilController.php
+++ app/controllers/PerfilController.php
@@ -25,4 +25,79 @@ class PerfilController extends \HXPHP\System\Controller
 				->setTemplate(true)
 				->setVar('user', User::find($user_id));
 	}
+
+	public function editarAction() 
+	{
+		$this->view->setFile('editar');
+
+		$user_id = $this->auth->getUserId(); // Obtendo atributos do usuário
+
+		$this->request->setCustomFilters(array(
+			'email' => FILTER_VALIDATE_EMAIL
+		));
+		
+		$post = $this->request->post();
+
+		if(!empty($post)) {
+			$atualizarUsuario = User::atualizar($user_id, $post);
+			
+			if ($atualizarUsuario->status === false) {
+				$this->load('Helpers\Alert', array(
+					'error',
+					'Não foi possível atualizar seu perfil. Verifique os erros abaixos:',
+					$atualizarUsuario->errors
+				));
+			}
+			else {
+				if (isset($_FILES['image']) && !empty($_FILES['image']['tmp_name'])) {
+					$uploadUserImage = new upload($_FILES['image']);
+
+					if ($uploadUserImage->uploaded) {
+						$image_name = md5(uniqid());
+
+						$uploadUserImage->file_new_name_body = $image_name;
+						$uploadUserImage->file_new_name_ext = 'png';
+						$uploadUserImage->resize = true;
+						$uploadUserImage->image_x = 200;
+						$uploadUserImage->image_ratio_y = true;
+
+						$dir_path = ROOT_PATH . DS . 'public' . DS . 'uploads' . DS . 'users' . DS . $atualizarUsuario->user->id . DS;
+						$uploadUserImage->Process($dir_path);
+
+						if ($uploadUserImage->processed) {
+							$uploadUserImage->Clean();
+							$this->load('Helpers\Alert', array(
+								'success',
+								'Perfil atualizado com sucesso.'
+							));
+
+							if (!is_null($atualizarUsuario->user->image)) {
+								unlink($dir_path . $atualizarUsuario->user->image);
+							}
+
+							$atualizarUsuario->user->image = $image_name . '.png';
+
+							$atualizarUsuario->user->save(false);
+						}
+						else {
+							$this->load('Helpers\Alert', array(
+								'error',
+								'Ops! Não foi possível atualizar sua imagem de perfil.',
+								$uploadUserImage->error
+							));
+						}
+					}
+				}
+				else {
+					$this->load('Helpers\Alert', array(
+						'success',
+						'Uhull! Perfil atualizado com sucesso.'
+					));
+				}
+
+				$this->view->setVar('user', $atualizarUsuario->user);
+				
+			}
+		}
+	}
 }
--- app/models/User.php
+++ app/models/User.php
@@ -75,6 +75,64 @@ class User extends \HXPHP\System\Model
 		return $callbackObj;
 	}
 
+	public static function atualizar($user_id, array $post)
+	{
+		$callbackObj = new \stdClass; // Atribuindo classe vazio do framework
+		$callbackObj->user = null;
+		$callbackObj->status = false;
+		$callbackObj->errors = array();
+
+		if ( isset($post['password']) && !empty($post['password']) ) {
+			//Função interna do framework para criptorafia
+			$password = \HXPHP\System\Tools::hashHX($post['password']); 
+
+			$post = array_merge($post, $password);
+		}
+
+		$user = self::find($user_id);
+
+		$user->name = $post['name'];
+		$user->username = $post['username'];
+		$user->email = $post['email'];
+
+		if (isset($post['salt'])) {
+			$user->password = $post['password'];
+			$user->salt = $post['salt'];
+		}
+
+		$exists_mail = self::find_by_email($post['email']);
+
+		if (!is_null($exists_mail) && intval($user_id) !== intval($exists_mail->id)) {
+			array_push($callbackObj->errors, 'Já existe um usuário com esse e-mail cadastrado. Por favor, escolha outro e tente novamente.');
+
+			return $callbackObj;
+		}
+
+		$exists_username = self::find_by_username($post['username']);
+
+		if (!is_null($exists_username) && intval($user_id) !== intval($exists_username->id)) {
+			array_push($callbackObj->errors, 'Já existe um usuário com o login < ' . $post['username'] . ' > cadastrado. Por favor, escolha outro e tente novamente.');
+
+			return $callbackObj;
+		}
+
+		$atualizar = $user->save(false);
+
+		if($atualizar) {
+			$callbackObj->user = $user;
+			$callbackObj->status = true;
+			return $callbackObj;
+		}
+
+		$errors = $cadastrar->errors->get_raw_errors(); 
+
+		foreach ($errors as $field => $message) {
+			array_push($callbackObj->errors, $message[0]);
+		}
+
+		return $callbackObj;
+	}
+
 	public static function login(array $post)
 	{
 		$callbackObj = new \stdClass; // Atribuindo classe vazio do framework
