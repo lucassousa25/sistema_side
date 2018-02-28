c94d6133c74cf0387e30c4e263fea2b4fd4f9b1f
Lucas S. Oliveira <lucassousao2510@gmail.com>
2018-02-28 17:28:01 -0300

    implementação da função listagem de produtos

--- app/controllers/ProdutosController.php
+++ app/controllers/ProdutosController.php
@@ -18,16 +18,52 @@ class ProdutosController extends \HXPHP\System\Controller
 		$user_id = $this->auth->getUserId(); // Obtendo atributos do usuário
 		$user = User::find($user_id);
 
-		$this->view->setVar('user', $user);
-	}
-
-	public function indexAction()
-	{
-		$this->view->setFile('listar')
+		$this->view->setVar('user', $user)
 				->setHeader('header_side')
 				->setFooter('footer_side')
 				->setTemplate(true)
 				->setTitle('SIDE | Produtos');
+	}
+
+	public function indexAction()
+	{
+		$this->view->setVar('products', Product::all())
+				->setFile('listar');
+	}
+
+	public function cadastrarAction()
+	{
+		$this->view->setFile('cadastrar');
+		
+		$this->request->setCustomFilters(array(
+			'cost' => FILTER_SANITIZE_NUMBER_FLOAT,
+			'sell_value' => FILTER_SANITIZE_NUMBER_FLOAT
+		));
+
+		$user_id = $this->auth->getUserId(); // Obtendo atributos do usuário
+
+		$post = $this->request->post();
+
+		if(!empty($post)) {
+			$cadastrarProduto = Product::cadastrar($post, $user_id);
+
+			if ($cadastrarProduto->status === false) {
+				$this->load('Helpers\Alert', array(
+					'error',
+					'Não foi possível efetuar seu cadastro. Verifique os erros abaixos:',
+					$cadastrarProduto->errors
+				));
+			}
+			else {
+				$this->view->setVar('products', Product::all())
+							->setFile('listar');
+
+				$this->load('Helpers\Alert', array(
+					'success',
+					'O produto ' . $cadastrarProduto->product_description . ' foi cadastrado no sistema!'
+				));
+			}
+		}
 	}
 
 	public function cadastrarProdutoAction()
--- app/models/Product.php
+++ app/models/Product.php
@@ -2,5 +2,70 @@
 
 class Product extends \HXPHP\System\Model
 {
+	// Validando a presença dos campos (phpActiveRecord) 
+	static $validates_presence_of = array(
+		array(
+			'description',
+			'message' => 'A descrição é um campo Obrigatório!'
+		),
+		array(
+			'sell_value',
+			'message' => 'O valor de venda é um campo Obrigatório!'
+		),
+		array(
+			'est_minimo',
+			'message' => 'O estoque mínimo é um campo Obrigatório!'
+		),
+		array(
+			'est_atual',
+			'message' => 'O estoque atual é um campo Obrigatório!'
+		)
+	);
 
+	// Validando exclusividades dos campos (phpActiveRecord)
+	static $validates_uniqueness_of = array(
+        array(
+       		'description', 
+       		'message' => 'Já existe um produto com essa descrição.'
+       	)
+    );
+
+	public static function cadastrar(array $post, $user_id)
+	{
+		$callbackObj = new \stdClass; // Atribuindo classe vazio do framework
+		$callbackObj->user = null;
+		$callbackObj->status = false;
+		$callbackObj->errors = array();
+		$callbackObj->product_description = null; // armazenando nome do produto para retorno no controller
+
+		// user_id | desription | internal_code | cost | sell_value | est_inicial | est_minimo | est_maximo
+		// est_atual | data_entrada | provider
+
+		$user_id_array = [
+			'user_id' => $user_id
+		];
+
+
+		$data_entrada = [
+			'data_entrada' => date('Y-m-d h:i:s')
+		];
+
+		$post = array_merge($user_id_array, $post, $data_entrada);
+
+		$cadastrar = self::create($post);
+
+		if($cadastrar->is_valid()) {
+			$callbackObj->user = $cadastrar;
+			$callbackObj->status = true;
+			$callbackObj->product_description = $post['description'];
+			return $callbackObj;
+		}
+
+		$errors = $cadastrar->errors->get_raw_errors(); 
+		
+		foreach ($errors as $field => $message) {
+			array_push($callbackObj->errors, $message[0]);
+		}
+		return $callbackObj;
+	}
 }
