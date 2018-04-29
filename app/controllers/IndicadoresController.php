445b9166a531354173c8d2aeff4292950806d855
Lucas S. Oliveira <lucassousao2510@gmail.com>
2018-04-29 13:45:38 -0300

    Adição do módulo de indicadores para avaliação

--- app/controllers/IndicadoresController.php
+++ app/controllers/IndicadoresController.php
@@ -17,17 +17,41 @@ class IndicadoresController extends \HXPHP\System\Controller
 
 		$user_id = $this->auth->getUserId(); // Obtendo atributos do usuário
 		$user = User::find($user_id);
+
+		$this->view->setVar('user', $user)
+				->setHeader('header_side')
+				->setFooter('footer_side')
+				->setTemplate(true)
+				->setTitle('SIDE | Indicadores');
 	}
 
 	public function indexAction()
 	{
-		
+		$this->view->setFile('listar');
 	}
 
 	public function geraIndicadorAction(int $product_id = 1)
 	{
 		if (intval($product_id)) {
-			Indicator::gerarIndicadores($product_id);
+			$registrarIndicadores = Indicator::gerarIndicadores($product_id);
+
+			if ($registrarIndicadores->status === false) {
+				$this->load('Helpers\Alert', array(
+					'error',
+					'Não foi possível gerar os Indicadores. Verifique os erros abaixos:',
+					$cadastrarProduto->errors
+				));
+			}
+			else {
+				$this->view->setVar('products', Product::find('all', array('limit' => '10', 'offset' => '0')))
+							->setFile('listar'); # Redirecinando para página de listagem
+
+				$this->load('Helpers\Alert', array(
+					'success',
+					'Giro de Estoque: O estoque girou ' . $registrarIndicadores->indicators['giro_estoque'] . 
+					'\n Cobertura de Estoque: O Estoque cobrirá ' . $registrarIndicadores->indicators['cobertura_estoque'] . ' dias.'
+				));
+			}
 		}
 	}
 }
--- app/models/Indicator.php
+++ app/models/Indicator.php
@@ -21,21 +21,67 @@ class Indicator extends \HXPHP\System\Model
 			$giroEstoque = null;
 			$coberturaEstoque = null;
 
+			// Variaveis de inserção no Banco
+			$registrarGiro = null;
+			$registrarCobertura = null;
+
+			$dateInsert = date('Y-m-d H:i:s');
+
 			foreach ($sell as $linha) {
 				if (date_format($linha->date_sell, 'm') == date('m')) // Verifica se está no mês atual
 					$totalVendas += $linha->quantity;
 			}
 			
 			$mediaEstoque = (($product->est_inicial + $product->est_atual) / 2);
-			$giroEstoque = number_format(($totalVendas / $mediaEstoque), 2, ',', '.');
-			$callbackObj->indicators['giro_estoque'] = $giroEstoque;
+			$giroEstoque = number_format(($totalVendas / $mediaEstoque), 2, '.', ',');
+			
 
 			$mediaVendas = number_format(($totalVendas / date('d')), 1, '.', ',');
 			$coberturaEstoque = number_format(($product->est_atual / $mediaVendas), 2, ',', '.');
-			$callbackObj->indicators['cobertura_estoque'] = $coberturaEstoque;
 			
-			var_dump($callbackObj->indicators);
-			die();
+
+			$array_indicator = [
+				'product_id'  => null,
+				'description' => null,
+				'value'		  => null,
+				'date_insert' => null	
+			];
+
+			if (!empty($giroEstoque)) {
+				$array_indicator['product_id'] = $product_id;
+				$array_indicator['description'] = 'Giro de Estoque';
+				$array_indicator['value'] = $giroEstoque;
+				$array_indicator['date_insert'] = $dateInsert;
+
+				$registrarGiro = self::create($array_indicator);
+			}
+
+
+			if (!empty($coberturaEstoque)) {
+				$array_indicator['product_id'] = $product_id;
+				$array_indicator['description'] = 'Cobertura de Estoque';
+				$array_indicator['value'] = intval($coberturaEstoque);
+				$array_indicator['date_insert'] = $dateInsert;
+
+				$registrarCobertura = self::create($array_indicator);
+			}
+
+			if($registrarGiro->is_valid() && $registrarCobertura->is_valid()) {
+				$callbackObj->user = $coberturaEstoque;
+				$callbackObj->status = true;
+				$callbackObj->indicators['giro_estoque'] = $giroEstoque;
+				$callbackObj->indicators['cobertura_estoque'] = intval($coberturaEstoque);
+
+				return $callbackObj;
+			}
+			else {
+				$errors = $cadastrar->errors->get_raw_errors(); 
+		
+				foreach ($errors as $field => $message) {
+					array_push($callbackObj->errors, $message[0]);
+				}
+				return $callbackObj;
+			}
 
 		} else {	
 			echo "não há vendas registradas desse produto!";
