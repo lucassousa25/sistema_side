4afa3e79f01a487b236baa9eb91982dd865e4205
Lucassousa25 <lucassousao2510@gmail.com>
2018-06-04 23:22:18 -0300

    Iniciando implementação de dados na home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -33,6 +33,39 @@ class HomeController extends \HXPHP\System\Controller
 
 	public function indexAction()
 	{	
-				
+		$this->view->setFile('index');
+
+		$maiorData = Parameter::find('first', array('select' => 'DISTINCT(left(date,7)) as data', 'order' => 'date desc'));
+		$produtos_est_minimo = Indicator::all(array('select' => 'product_id, estoque_minimo, ponto_reposicao', 'conditions' => "date LIKE '%$maiorData->data%'"));
+		$produtos_est_atual = Parameter::all(array('select' => 'product_id, estoque_atual', 'conditions' => "date LIKE '%$maiorData->data%'"));
+
+		$produtos_abaixo_est_minimo = null;
+		$produtos_abaixo_ponto_reposicao = null;
+		$produtos_prox_ponto_reposicao = null;
+
+		foreach ($produtos_est_minimo as $linha1) :
+			foreach ($produtos_est_atual as $linha2) {
+				if ($linha2->product_id == $linha1->product_id) {
+
+					if ($linha2->estoque_atual < $linha1->estoque_minimo) {
+						$produtos_abaixo_est_minimo++;
+					}
+					elseif (($linha2->estoque_atual >= $linha1->estoque_minimo) && ($linha2->estoque_atual < $linha1->ponto_reposicao)) {
+						$produtos_abaixo_ponto_reposicao++;
+					}
+					elseif (($linha2->estoque_atual >= $linha1->ponto_reposicao) && ($linha2->estoque_atual < ($linha1->ponto_reposicao + ($linha1->ponto_reposicao * 0.2)))) {
+					 	$produtos_prox_ponto_reposicao++;
+					}
+
+				}
+			}
+		endforeach;
+
+		$this->view->setFile('index')
+               ->setVars([
+               		'produtos_abaixo_est_minimo' => $produtos_abaixo_est_minimo,
+               		'produtos_abaixo_ponto_reposicao' => $produtos_abaixo_ponto_reposicao,
+               		'produtos_prox_ponto_reposicao' => $produtos_prox_ponto_reposicao
+               		]);
 	}
 }
